f972faf11599b2c50f8f5f6878ddd98f1bc1e3ff
Senitha-EminentB <203494039+Senitha-EminentB@users.noreply.github.com>
2025-05-01 19:33:49 +0530

    Import Routes
    Import Routes auth and User

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connection = require("./db");
-
+const userRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
 
 connection();
 
@@ -11,9 +12,8 @@ connection();
 app.use(express.json());
 app.use(cors());
 
-// // routes
-// app.use("/api/users", userRoutes);
-// app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT;
 app.listen(port, console.log(`Listening on port ${port}...`));
