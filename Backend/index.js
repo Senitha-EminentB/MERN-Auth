64ded50e0614c4c60db4aae090fb0715a44e9627
Senitha-EminentB <203494039+Senitha-EminentB@users.noreply.github.com>
2025-05-01 18:52:16 +0530

    Create Backend
    Create Backend and install Dependences and implement  the DB

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connection = require("./db");
+
+
+connection();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// // routes
+// app.use("/api/users", userRoutes);
+// app.use("/api/auth", authRoutes);
+
+const port = process.env.PORT;
+app.listen(port, console.log(`Listening on port ${port}...`));
