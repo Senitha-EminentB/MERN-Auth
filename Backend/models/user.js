7e2d271bf512219dc52a2a1ba4772c1f35eefe72
Senitha-EminentB <203494039+Senitha-EminentB@users.noreply.github.com>
2025-05-01 19:14:08 +0530

    Create UserModel
    Create UserModel and Implmenting the JWTs

---
+++ Backend/models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const joi = require('joi');
+const passwordComplexity = require("joi-password-complexity");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    companyName: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    }
+    
+});
+
+userSchema.method.generateAuthToken = function() {
+    const token = jwt.sign({_id: this._id}, process.env.JWT_PRIVATE_KEY,{expiresIn: "7d"});
+    return token;
+}
+
+module.exports = mongoose.model('User', userSchema);
+
+const validate = (data) => {
+	const schema = Joi.object({
+		name: Joi.string().required().label("Name"),
+		email: Joi.string().email().required().label("Email"),
+		password: passwordComplexity().required().label("Password"),
+        companyName: Joi.string().required().label("Company Name"),
+        phone: Joi.string().required().label("Phone")
+	});
+	return schema.validate(data);
+};
+
+module.exports = { User, validate };
